e069318ee7558f741bf98944a045a353fea8d019
Ayan Koley <koleyayan07@gmail.com>
2026-09-12 18:16:01 +0530

    REFINE: add refresh route to auth routes for token renewal

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { signInUser, signOutUser, signUpUser } from '../controllers/auth.controllers.js';
+import { refreshAccessToken, signInUser, signOutUser, signUpUser } from '../controllers/auth.controllers.js';
 import { verifyJWT } from '../middlewares/verifyJWT.js';
 import { authenticateRefreshToken } from '../middlewares/authenticateRefreshToken.js';
 
@@ -8,6 +8,7 @@ const router = Router();
 
 router.route("/signup").post(signUpUser);
 router.route("/signin").post(signInUser);
-router.route("/signout").post(verifyJWT, authenticateRefreshToken, signOutUser)
+router.route("/signout").post(verifyJWT, authenticateRefreshToken, signOutUser);
+router.route("/refresh").post(authenticateRefreshToken, refreshAccessToken);
 
 export default router;
