88ac8e68ee64d3d5367b27402dc2a94bee77590e
Ayan Koley <koleyayan07@gmail.com>
2026-09-13 20:01:22 +0530

    IMPLEMENT: add message validation schema using Zod

---
+++ server/src/validations/message.validations.ts
@@ -0,0 +1,13 @@
+import * as z from 'zod';
+
+const messageValidation = z.object({
+    conversationId: z.number(),
+    senderId: z.number(),
+    content: z.string().trim()
+});
+
+
+
+export {
+    messageValidation
+}
