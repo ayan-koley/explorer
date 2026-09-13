35c53b3311c37f22f7c037bef9dc2a1b82aa7f19
Ayan Koley <koleyayan07@gmail.com>
2026-09-13 20:01:56 +0530

    IMPLEMENT: add createConversation controller for managing direct conversations

---
+++ server/src/controllers/conversation.controllers.ts
@@ -0,0 +1,29 @@
+import { conversationService } from "../services/conversation.services.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Request, Response } from "express";
+
+const createConversation = asyncHandler(async(req: Request, res: Response) => {
+    if(!req.user) {
+        return res.status(400).json(
+            ApiResponse.error("unauthorized user")
+        )
+    }
+    const userId1 = req.user?.id;
+    const {user_id: userId2} = req.body;
+
+    if(!userId2) {
+        return res.status(400).json(
+            ApiResponse.error("user id is missing")
+        )
+    }
+    const conversation = await conversationService.findOrCreateDirectConversation(userId1, userId2)
+
+    return res.status(200).json(
+        ApiResponse.success(conversation, "Conversation created successfully")
+    )
+})
+
+export {
+    createConversation
+}
