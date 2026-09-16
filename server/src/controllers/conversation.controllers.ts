b228212bede68712714847030afcd73b680918af
Ayan Koley <koleyayan07@gmail.com>
2026-09-16 17:36:15 +0530

    IMPLEMENT: add methods for retrieving user conversations and conversation by ID

--- server/src/controllers/conversation.controllers.ts
+++ server/src/controllers/conversation.controllers.ts
@@ -24,6 +24,46 @@ const createConversation = asyncHandler(async(req: Request, res: Response) => {
     )
 })
 
+const getUserConversations = asyncHandler(async(req: Request, res: Response) => {
+    if(!req.user) {
+        return res.status(400).json(
+            ApiResponse.error("unauthorized user")
+        )
+    }
+
+    const conversations = await conversationService.getConversationsOfUser(req.user.id);
+
+    return res.status(200).json(
+        ApiResponse.success(conversations, "Fetch all conversations successfully")
+    )
+})
+
+const getConversationById = asyncHandler(async(req: Request, res: Response) => {
+    if(!req.user) {
+        return res.status(400).json(
+            ApiResponse.error("unauthorized user")
+        )
+    }
+
+    const {conversationId} = req.params;
+
+    const convId = Number(conversationId);
+
+    if(!convId) {
+        return res.status(404).json(
+            ApiResponse.error("Conversation id is missing")
+        )
+    }
+
+    const conversation = await conversationService.getConversationById(convId);
+
+    return res.status(200).json(
+        ApiResponse.success(conversation, "Successfully fetch the conversation")
+    )
+})
+
 export {
-    createConversation
+    createConversation,
+    getUserConversations,
+    getConversationById
 }
