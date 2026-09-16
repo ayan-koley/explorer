5bf530b611c035f43e4e77cb54372fda155f35d1
Ayan Koley <koleyayan07@gmail.com>
2026-09-16 17:36:04 +0530

    IMPLEMENT: add routes for retrieving conversations by ID and user conversations

--- server/src/routes/conversations.routes.ts
+++ server/src/routes/conversations.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { verifyJWT } from '../middlewares/verifyJWT.js';
-import { createConversation } from '../controllers/conversation.controllers.js';
+import { createConversation, getConversationById, getUserConversations } from '../controllers/conversation.controllers.js';
 
 const router = Router();
 
 
+router.route("/").get(verifyJWT, getUserConversations);
+router.route("/:conversationId").get(verifyJWT, getConversationById);
 router.route("/direct").post(verifyJWT, createConversation);
 
 export default router;
