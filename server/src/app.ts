197ef9d790fcf96775efde2202bbad2def335ac2
Ayan Koley <koleyayan07@gmail.com>
2026-09-19 07:36:30 +0530

    feat: add additional routes for servers, channels, and server members

--- server/src/app.ts
+++ server/src/app.ts
@@ -21,6 +21,10 @@ app.use(cookieParser())
 import authRoutes from './routes/auth.routes.js';
 import conversationsRoutes from './routes/conversations.routes.js';
 import messagesRoutes from './routes/message.routes.js';
+import serverRoutes from './routes/server.routes.js';
+import channelRoutes from './routes/channel.routes.js';
+import channelMessageRoutes from './routes/channel-message.routes.js';
+import serverMemberRoutes from './routes/server-member.routes.js';
 
 app.use("/api/v1/healthcheck", (req: Request, res: Response) => {
     return res.status(200).json({message: "work properly "})
@@ -28,5 +32,9 @@ app.use("/api/v1/healthcheck", (req: Request, res: Response) => {
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/conversations", conversationsRoutes);
 app.use("/api/v1/messages", messagesRoutes);
+app.use("/api/v1/servers", serverRoutes);
+app.use("/api/v1/channels", channelRoutes);
+app.use("/api/v1/channel-messages", channelMessageRoutes);
+app.use("/api/v1/server-members", serverMemberRoutes);
 
 export default app;
