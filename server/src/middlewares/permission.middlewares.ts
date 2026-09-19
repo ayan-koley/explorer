ab0468f47cff5ebfa5f331bea1adc8597ead15d7
Ayan Koley <koleyayan07@gmail.com>
2026-09-19 07:35:56 +0530

    feat: add permission middleware with basic permission functions

---
+++ server/src/middlewares/permission.middlewares.ts
@@ -0,0 +1,12 @@
+export const requirePermission = () => {}
+export const hasPermission = () => {}
+
+/**
+ * VIEW_CHANNEL
+ * SEND_MESSAGES
+ * MANAGE_MESSAGES
+ * MANAGE_CHANNELS
+ * MANAGE_SERVER
+ * CONNECT
+ * SPEAK
+ */
